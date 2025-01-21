10d6078467b647c42283f01c0d2f2349f6b9aaef
Harish Suresh <harishsuresh@Harishs-MacBook-Pro.local>
2025-01-21 02:00:25 -0500

    lab1 challenge completed

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -3,12 +3,11 @@ package com.example.lab1
 import android.os.Bundle
 import android.widget.Button
 import android.view.View
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity as AppCompatActivity1
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
-
-class MainActivity : AppCompatActivity1() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
@@ -21,12 +20,11 @@ class MainActivity : AppCompatActivity1() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener {
-            view: View ->
-            Toast.makeText(
-                this,
+        trueButton.setOnClickListener { view: View ->
+            Snackbar.make(
+                view,
                 R.string.correct_toast,
-                Toast.LENGTH_SHORT
+                Snackbar.LENGTH_SHORT
             ).show()
         }
     }
